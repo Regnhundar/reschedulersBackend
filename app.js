d4462752b33a8e47056a477693959d77ba96717b
Magnus Nilsson <regnhundar@gmail.com>
2024-05-29 16:07:27 +0200

    Skapade upp db för users, route för auth, skapade post-req för att skapa ett konto, userSchema via joi.

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import errorHandler from "./middleware/errorHandler.js";
+import authenticationRoute from "./routes/auth.js"
 
 const app = express();
 const port = 1337;
 
 //Middleware
 app.use(express.json());
-
+app.use('/auth', authenticationRoute);
 
 app.listen(port, () => console.log(`Server running on ${port}`));
 app.use(errorHandler);
