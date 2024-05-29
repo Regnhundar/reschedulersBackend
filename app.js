20a83e263793cd6642e3aa66b666cd1253fd141e
Lisa Tetzlaff <lisa.emma.tetzlaff@gmail.com>
2024-05-29 15:02:40 +0200

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import aboutRouter from './routes/about.js';
 
 const app = express();
 const port = 1337;
@@ -6,5 +7,7 @@ const port = 1337;
 //Middleware
 app.use(express.json());
 
+// Routes
+app.use('/about', aboutRouter);
 
 app.listen(port, () => console.log(`Server running on ${port}`));
---
+++ routes/about.js
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import Datastore from 'nedb-promises';
+
+const router = Router();
+
+// Skapa en databas för about 
+const db = new Datastore({ filename: './data/about.db', autoload: true });
+
+// Example: Insert text om databasen är tom med en catch
+db.count({})
+    .then(count => {
+        if (count === 0) {
+            db.insert({ text: 'Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio, grounds dripper, crema, strong whipped, variety extra iced id lungo half and half mazagran. Pumpkin spice.' });
+        }
+    })
+    .catch(err => {
+        console.error('Error initializing database:', err);
+    });
+
+// GET about
+router.get('/', async (req, res) => {
+    try {
+        //Hämtar dokument i databasen
+        const docs = await db.find({});
+        // Om hämtningen lyckas, returnera dokumenten som JSON-svar.
+        res.json(docs);
+    } catch (error) {
+        //Om något går fel catchas detta ocg skickar error
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+export default router;
